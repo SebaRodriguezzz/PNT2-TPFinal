2f48c9ab5ef4ce7589941cbf6cc8c06f0f7762c4
SebaRodriguezzz <sebas.r1070@gmail.com>
2023-11-01 20:28:54 -0300

    Router actualizado

--- TPFinal-PNT2/src/router/index.js
+++ TPFinal-PNT2/src/router/index.js
@@ -1,18 +1,33 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router'
 import HomeView from '../views/HomeView.vue'
+import SystemView from '../views/SystemView.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
 import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/home',
+      path: '/',
       name: 'home',
       component: HomeView
     },
     {
-      path: '/',
-      name: 'login',
+      path: '/system',
+      name: 'systemview',
+      component: SystemView,
+      meta: { RequireAuth: true}
+    },
+    {
+      path: '/login',
+      name: 'loginview',
       component: LoginView
+    },
+    {
+      path: '/shopping',
+      name: 'shopping',
+      component: ShoppingCart
     },
     {
       path: '/about',
@@ -22,8 +37,20 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
-    
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
   ]
 })
 
+router.beforeEach( (to,from,next) => {
+  const usuarioLog = localStorage.getItem('usuario')
+  if( to.matched.some(r => r.meta.RequireAuth) && !usuarioLog ) {
+    next('/')
+  }
+  next()
+})
+  
 export default router
